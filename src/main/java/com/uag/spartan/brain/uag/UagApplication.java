b1e6ad0b6cc78e3acbe5c7d764a8020da4696f87
Omar Cervantes <omarcervantes_vega@hotmail.com>
2020-08-20 22:46:24 -0500

    Adding iles

---
+++ src/main/java/com/uag/spartan/brain/uag/UagApplication.java
@@ -0,0 +1,13 @@
+package com.uag.spartan.brain.uag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uag/spartan/brain/uag/UagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uag.spartan.brain.uag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
