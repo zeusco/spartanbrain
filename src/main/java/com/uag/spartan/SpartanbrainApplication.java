dc7917abe5f648a526c76037187aa336024c7f3c
Omar Cervantes <omarcervantes_vega@hotmail.com>
2022-04-27 23:21:31 -0500

    27/04/2022

---
+++ src/main/java/com/uag/spartan/SpartanbrainApplication.java
@@ -0,0 +1,13 @@
+package com.uag.spartan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartanbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanbrainApplication.class, args);
+	}
+
+}
